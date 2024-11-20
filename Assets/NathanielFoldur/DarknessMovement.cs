c2003feaebb35858bece96004ac840cdbb081fd9
Altorook <nathanberry403@gmail.com>
2024-11-20 16:46:54 -0700

    Darkness can move

---
+++ Assets/NathanielFoldur/DarknessMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessMovement : MonoBehaviour
+{
+    public float DarknessSpeed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 DarkPos = new Vector2(DarknessSpeed,0);
+        transform.position += (Vector3) DarkPos;
+    }
+}
