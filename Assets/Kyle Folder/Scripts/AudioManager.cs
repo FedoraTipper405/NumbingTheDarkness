f19da7fe57c1488034eb60347600817d5615b957
Kyle.C <k.chudobiak271@mybvc.ca>
2024-11-21 21:22:57 -0700

    Added Audio Manager

---
+++ Assets/Kyle Folder/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundClip
+{
+   
+}
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] soundClip;
+    [SerializeField]
+    public AudioSource SFXSource;
+    [SerializeField]
+    public AudioSource MusicSource;
+    [SerializeField]
+    private AudioClip musicClip;
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        MusicSource.clip = musicClip;
+        MusicSource.Play();
+    }
+
+    public static void PlaySound(SoundClip soundClip)
+    {
+        instance.SFXSource.PlayOneShot(instance.soundClip[(int)soundClip]);
+    }
+}
