b2335296dfa396769e011335c23924ddcd731d74
Dustin Oakley <dustin@oakleyi.com>
2024-11-25 04:04:41 -0700

    Added Main Menu and Snap Script
    Also some Shader stuff for le Darkness, don't worry I'll speedrun making the level layouts

---
+++ Assets/Dustin Folder/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Tooltip("Name of the scene to load when Start is clicked.")]
+    public string sceneToLoad = "GameScene"; // Default to "GameScene", can be changed in the Inspector
+
+    public void StartGame()
+    {
+        // Check if the scene name is valid before attempting to load
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogError("No scene name specified for StartGame.");
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
